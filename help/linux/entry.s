5912a2310f60ccc76b15879243e1e113d1e05d10
Linus Dikomey <l.dikomey03@gmail.com>
2022-08-19 23:56:12 +0200

    updated linux entry file
    - pass through main result again as it now correctly returns exit codes
    - fix linker warning by adding .note.GNU-stack section

--- help/linux/entry.s
+++ help/linux/entry.s
@@ -3,18 +3,10 @@ extern exit ; from libc
 
 global _start
 
+section .note.GNU-stack
 section .text
 
 _start:
   call main
-  
-  ; mov rdi, rax ; mov result of main into the exit result
-  ; mov rax, 60       ; exit(
-  ; syscall           ; );
-
-  mov rdi, 0 ; set exit code to 0
+  mov rdi, rax ; mov result of main into the exit result
   call exit ; better than a raw syscall because it flushes io buffers
-
-section .rodata
-  msg: db "Eyelang entry point", 10
-  msglen: equ $ - msg
